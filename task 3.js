15335501a7f552e81f6ebbb9f1d8709dc0f66d82
BOJJUSHIREESHA <140920428+BOJJUSHIREESHA@users.noreply.github.com>
2026-04-30 17:34:56 +0530

    Update task 3.js

--- task 3.js
+++ task 3.js
@@ -1,11 +1,15 @@
 // LEVEL 1: FUNCTION BASCIS
-
 // Task 1: Student Form Function
-function studentForm(name, age, course) {
-    console.log("Name:", name, "Age:", age, "Course:", course);
+function studentform(name, age, course){
+    console.log( "Name: " + name);
+    console.log( "age: " + age);
+    console.log( "course: "+ course);
 }
-
-studentForm("Naveen", 22, "MERN");
+studentform("Naveen", 22, "MERN");
+   // Output: 
+// Name: Naveen
+// age: 22
+// course: MERN 
 
 
 // Task 2: Calculator Function
@@ -16,26 +20,32 @@ function calc(a, b) {
 }
 
 calc(10, 5);
+   // Output: 
+//Addition: 15
+// Subtraction: 5
+//Multiplication: 50 
 
 
 // Task 3:Reusable Greeting
 function greet(name) {
     console.log("Hello", name);
 }
-
 greet("Kamal");
 greet("Praveen");
 greet("Sai");
+   // Output: 
+// Hello Kamal
+// Hello Praveen
+// Hello Sai
 
 
 // LEVEL 2: Return & Scope
-
 // Task 4: Return Value
 function square(num) {
     return num * num;
 }
-
 console.log(square(5)); // 25
+    // Output:25
 
 
 // Task 5: Scope Check
@@ -44,26 +54,29 @@ function testScope() {
      secret = "javascript";
 }   
 testScope();
-
 console.log(secret); 
+  // Output: ReferenceError
 
 //Why this happens:
-
 // let has block scope
 // The variable secret exists only inside the function
 // Outside the function, it is not accessible.
 
 
 // Level 3 – Spread / Rest
-
 // Task 6: Merge Arrays (Spread Operator)
 let boys = ["car", "bike"];
 let girls = ["doll", "teddy"];
 
-
 let toys = [...boys, ...girls];
-
 console.log(toys);
+   // Output:
+// Array(4)0: "car"
+// 1: "bike"   
+// 2: "doll"
+// 3: "teddy"
+// length: 4
+// [[Prototype]]: Array(0)
 
 
 // Task 7: Unlimited Numbers (Rest Operator)
@@ -73,21 +86,18 @@ function sumAll(...nums) {
     for (let num of nums) {
         total += num;
     }
-
     console.log(total);
 }
-
 sumAll(10, 20, 30, 40);
+   // Output: 100
 
 
 // Level 4 – Destructuring
-
 // Task 8: Array Destructuring
 let colors = ["red", "green", "blue"];
-
 let [c1, c2, c3] = colors;
-
 console.log(c1, c2, c3);
+    // Output: red green blue
 
 
 // Task 9: Object Destructuring
@@ -96,14 +106,12 @@ let emp = {
     role: "Developer",
     salary: "5LPA"
 };
-
 let { name, role } = emp;
-
 console.log(name, role);
+   // Output: Naveen Developer
 
 
 // Level 5 – Real-Time Logic
-
 // Task 10: Offer Generator
 function* offerGenerator() {
     yield "10% cashback";
@@ -112,7 +120,6 @@ function* offerGenerator() {
     yield "Try again";
     return "No more offers";
 }
-
 let offers = offerGenerator();
 
 console.log(offers.next().value);
@@ -120,10 +127,15 @@ console.log(offers.next().value);
 console.log(offers.next().value);
 console.log(offers.next().value);
 console.log(offers.next().value);
+    // Output: 
+//10% cashback
+//20% cashback
+//50% cashback
+//Try again
+//No more offers
 
 
 // Level 6 – Advanced
-
 // Task 11: Curry Function
 function add(a) {
     return function(b) {
@@ -134,6 +146,7 @@ function add(a) {
 }
 
 console.log(add(10)(20)(30));
+    // Output: 60
 
 
 // Task 12: Student Marks Analyzer
@@ -143,23 +156,25 @@ function marks(...nums) {
     for (let num of nums) {
         total += num;
     }
-
     let average = total / nums.length;
-
     console.log("Total =", total);
     console.log("Average =", average);
 }
-
 marks(80, 90, 70, 60);
+   // Output: 
+//Total = 300
+//Average = 75
 
 
 // Challenge Task (Real Company Level)
-
 // Employee Registration System
 function register(name, role, ...skills) {
     console.log("Name:", name);
     console.log("Role:", role);
     console.log("Skills:", skills.join(" "));
 }
-
 register("Naveen", "Frontend", "HTML", "CSS", "JS", "React");
+    // Output:
+//Name: Naveen
+//Role: Frontend
+//Skills: HTML CSS JS React
